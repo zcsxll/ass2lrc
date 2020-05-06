9914c7acd4b25bec96e45aa854f2a84c01cab2c4
zhaochengshuai <zhaochengshuai@sogou-inc.com>
2020-05-06 18:53:25 +0800

    add ass2lrc.py

---
+++ ass2lrc.py
@@ -0,0 +1,86 @@
+import os
+import re
+
+class Dialogue:
+    def __init__(self, sh=0, sm=0, ss=0.00, chinese='', englisg=''):
+        self.sh = sh
+        self.sm = sm
+        self.ss = ss
+        self.chinese = chinese
+        self.english = englisg
+
+    def set_ass(self, str_ass):
+        # print('[%s]' % str_ass)
+        ss = str_ass.split(',')
+        # print(ss)
+        assert len(ss) >= 10
+        self.get_start_time(ss[1])
+
+        origin = ss[9]
+        for s in ss[10:]:
+            origin += (",%s" % s)
+        self.get_content(origin)
+
+    def get_start_time(self, s):
+        ss = s.split(':')
+        # print(ss)
+        assert len(ss) == 3
+        self.sh = int(ss[0])
+        self.sm = int(ss[1])
+        self.ss = float(ss[2])
+
+    def get_content(self, s):
+        # print(s)
+        # s.replace('{*}', '')
+        # print(s)
+        # a = re.sub(u"\\(.*?\\)|\\{.*?}|\\[.*?]", "", s)
+        content = re.sub('{.*?\\}', '', s)
+        # print(content)
+        ss = content.split('\\N')
+        assert len(ss) <= 2
+        self.chinese = ss[0]
+        if len(ss) == 1:
+            self.english = ''            
+        else:
+            self.english = ss[1]
+
+    def get_time(self):
+        return self.sh * 60 * 60 + self.sm * 60 + self.ss
+
+    def get_lrc(self, off_s):
+        assert self.sh == 0
+        s = self.sm * 60 + self.ss + off_s
+        sm = s // 60
+        ss = s - sm * 60
+        ret = "[%d:%.2f]%s # %s" % (sm, ss, self.english, self.chinese)
+        return ret
+
+class Ass2Lrc:
+    def __init__(self):
+        self.dialogue = Dialogue()
+        self.off_s = -1
+
+    def process(self, file_in_ass, file_out_lrc):
+        print(file_in_ass)
+        pf = open(file_in_ass)
+        pf_lrc = open(file_out_lrc, mode='w')
+        for idx, line in enumerate(pf):
+            line = line.strip()
+            if not line.startswith('Dialogue:'):
+                continue
+            # print(line)
+            self.dialogue.set_ass(line[10:])
+            # if idx > 50:
+            #     break
+            if self.off_s < 0:
+                self.off_s = self.dialogue.get_time()
+            line = self.dialogue.get_lrc(-self.off_s)
+            print(line)
+            pf_lrc.write("%s\n" % line)
+        pf.close
+        pf_lrc.close()
+
+if __name__ == "__main__":
+    file = './Friends.ass/Friends.S01.1994/Friends.S01E01.1994.BluRay.1080p.x265.10bit.MNHD-FRDS.ass'
+    file_lrc = './Friends.lrc/Friends101.lrc'
+    Ass2Lrc().process(file, file_lrc)
